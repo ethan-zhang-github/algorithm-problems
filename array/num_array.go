9b3694a58dc254fdc6e03ba9e5ead1e72ee7f222
EthanZhang <yixiezi1994@gmail.com>
2024-02-07 10:18:11 +0800

    sum-range

--- array/num_array.go
+++ array/num_array.go
@@ -1,7 +1,8 @@
 package array
 
+// NumArray https://leetcode.cn/problems/range-sum-query-immutable/description/
 type NumArray struct {
-	SumArray []int
+	sumArray []int
 }
 
 func Constructor(nums []int) NumArray {
@@ -10,12 +11,12 @@ func Constructor(nums []int) NumArray {
 	for i := 1; i < len(nums); i++ {
 		sumArray[i] = sumArray[i-1] + nums[i]
 	}
-	return NumArray{SumArray: sumArray}
+	return NumArray{sumArray: sumArray}
 }
 
 func (this *NumArray) SumRange(left int, right int) int {
 	if left == 0 {
-		return this.SumArray[right]
+		return this.sumArray[right]
 	}
-	return this.SumArray[right] - this.SumArray[left-1]
+	return this.sumArray[right] - this.sumArray[left-1]
 }
