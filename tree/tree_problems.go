28e149ba68f86b8740fc44df0a0996b9f4b458a5
EthanZhang <yixiezi1994@gmail.com>
2024-03-19 18:01:42 +0800

    tree

--- tree/tree_problems.go
+++ tree/tree_problems.go
@@ -6,3 +6,17 @@ func maxDepth(root *TreeNode) int {
 	}
 	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
 }
+
+func preorderTraversal(root *TreeNode) []int {
+	return preorderTraversalRecursive(root, make([]int, 0))
+}
+
+func preorderTraversalRecursive(root *TreeNode, values []int) []int {
+	if root == nil {
+		return values
+	}
+	values = append(values, root.Val)
+	values = preorderTraversalRecursive(root.Left, values)
+	values = preorderTraversalRecursive(root.Right, values)
+	return values
+}
--- tree/tree_problems_test.go
+++ tree/tree_problems_test.go
@@ -9,3 +9,8 @@ func TestMaxDepth(t *testing.T) {
 	tree := Constructor([]int{3, 9, 20, NULL, NULL, 15, 7})
 	fmt.Println(maxDepth(tree))
 }
+
+func TestPreorderTraversal(t *testing.T) {
+	tree := Constructor([]int{3, 9, 20, NULL, NULL, 15, 7})
+	fmt.Println(preorderTraversal(tree))
+}
